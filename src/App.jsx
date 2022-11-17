be26ddaca725d83e04e0f983add4fec9e54fb58f
Allan Vinicius Coqueiro Silva <Allan.Silva1@kyndryl.com>
2022-11-17 17:59:59 -0300

    Mudanças:
    - configurado roteamento;
    - adaptação dos componentes de rota para a lib de UI;

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link as RouterLink
+} from 'react-router-dom';
+
+import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button'
+import Link from '@mui/material/Link';
+import TextField from '@mui/material/TextField'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <Button onClick={()=>{alert('Sou uma função')}}>Hello World!</Button>
-    </div>
+    <Typography>
+      <Router>
+
+        <Button component={RouterLink} to='/'>Home</Button>
+        <Button component={RouterLink} to='/teste'>Teste</Button>
+        <Link component={RouterLink}>Link comum</Link>
+
+        <Routes>
+          <Route path="" exact element={<Button variant='contained'>Hello World!</Button>} />
+          <Route path="/teste" exact element={<h1>pagina teste</h1>} />
+        </Routes>
+        <TextField label="Insira algo" variant="standard" />
+      </Router>
+    </Typography>
   )
 }
 
